8b05aba74a83908ed129d6a9efa04b066222795c
unknown <thuranszkyzso@gmail.com>
2023-03-03 11:35:20 +0100

    test

--- src/routes/Etchings.jsx
+++ src/routes/Etchings.jsx
@@ -1,13 +1,11 @@
 import DataDisplay from "../components/Datadisplay";
-import { useOutletContext } from "react-router-dom";
+
 
 const Etchings = () => {
 
-  // const {windowWidth} = useOutletContext()
-  // console.log("context : ",windowWidth)
 
 
-  const urlEnding = "etchings";
+  const urlEnding = "etching";
 
   return <DataDisplay {...{ urlEnding }} />;
 };
